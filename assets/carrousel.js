acf70081233f8ca66d305b2d62d785c99f3809a5
lucasmpalu <111322631+lucasmpalu@users.noreply.github.com>
2023-03-01 14:33:58 -0300

    first commit

---
+++ assets/carrousel.js
@@ -0,0 +1,22 @@
+
+$big = document.querySelector('.carrousel-big')
+$points = document.querySelectorAll('.point')
+
+
+$points.forEach((item, index) => {
+    $points[0].classList.add('activePoint')
+
+    $points[index].addEventListener('click', () => {
+        let position = index
+        let operation = position * -25
+        console.log($points)
+        $big.style.transform = `translateX(${operation}%)`
+        
+        $points.forEach((item, index )=>{
+            $points[index].classList.remove('activePoint')
+        })
+
+            $points[index].classList.add('activePoint')
+    })
+
+});
