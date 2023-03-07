1dd6a87a98441f511548b426b517bda03cc67b3f
lucasmpalu <111322631+lucasmpalu@users.noreply.github.com>
2023-03-07 16:54:56 -0300

    overlay

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,26 +3,34 @@ const $burgerMenu = document.querySelector('.burger-menu')
 const $overlay = document.querySelector('.overlay')
 
 const scrollOverlayOut = () => {
-    if ($overlay.classList.contains = 'displayBlock') {
+    if ($overlay.classList.contains('displayBlock')) {
         $burgerMenu.classList.remove('menu-active')
         $overlay.classList.remove('displayBlock')
     }
 }
 
 const clickOverlayOut = (e) => {
-    if ($overlay.classList.contains = 'displayBlock') {
+
+    if (e.target.classList.contains('close-menu')) {
         $burgerMenu.classList.remove('menu-active')
         $overlay.classList.remove('displayBlock')
     }
+
+    if(e.target.classList.contains('overlay')){
+        $burgerMenu.classList.remove('menu-active')
+        $overlay.classList.remove('displayBlock')
+    }
+
 }
 
 init = () => {
     
     $labelMenu.addEventListener('click',function() {
-        $burgerMenu.classList.toggle('menu-active')
-        $overlay.classList.toggle('displayBlock')
+        $burgerMenu.classList.add('menu-active')
+        $overlay.classList.add('displayBlock')
     })
     $burgerMenu.addEventListener('click', clickOverlayOut)
+    window.addEventListener('click', clickOverlayOut)
     window.addEventListener('scroll', scrollOverlayOut)
 }
 
